c25c04e66992089ecdd2e0e8f127f84a106df1e8
Ferin <ferin@localhost.localdomain>
2020-04-22 18:39:54 +0530

    Updates

---
+++ precache-manifest.beca105ddefa6d54b08a7efc70febfff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "629f9dcf27451de24f663bda9fef6c3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abcb9f7035e1c63884c9",
+    "url": "/static/css/main.9840d89d.chunk.css"
+  },
+  {
+    "revision": "62e5c4c0f691efb6a51e",
+    "url": "/static/js/2.89f5259e.chunk.js"
+  },
+  {
+    "revision": "d0b30a1909a753c393e134caf98fa9bb",
+    "url": "/static/js/2.89f5259e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abcb9f7035e1c63884c9",
+    "url": "/static/js/main.654017b9.chunk.js"
+  },
+  {
+    "revision": "e8b84128fce92fbe2daf",
+    "url": "/static/js/runtime-main.3048490e.js"
+  },
+  {
+    "revision": "9d34e568090aa85bdb724cd9d7f68fe5",
+    "url": "/static/media/final8085.9d34e568.svg"
+  }
+]);
