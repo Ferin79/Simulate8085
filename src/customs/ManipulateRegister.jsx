fd4551e2568140a69f3f30928194ce729fd3b8ef
Ferin <ferin@localhost.localdomain>
2020-04-23 19:10:08 +0530

    Register Manipulation Done

--- src/customs/ManipulateRegister.jsx
+++ src/customs/ManipulateRegister.jsx
@@ -3,70 +3,112 @@ import style from "./css/Register.module.css";
 import { Table, Button, Icon, TextInput } from "react-materialize";
 
 const ManipulateRegister = () => {
+  const [isErrorText, setisErrorText] = useState(false);
   const [RegisterData, setRegisterData] = useState([
     {
       id: 0,
       name: "W Register",
       value: 0,
       isEdit: false,
-      key: 0,
     },
     {
       id: 1,
       name: "Z Register",
       value: 0,
       isEdit: false,
-      key: 0,
     },
     {
       id: 2,
       name: "B Register",
       value: 0,
       isEdit: false,
-      key: 0,
     },
     {
       id: 3,
       name: "C Register",
       value: 0,
       isEdit: false,
-      key: 0,
     },
     {
       id: 4,
       name: "D Register",
       value: 0,
       isEdit: false,
-      key: 0,
     },
     {
       id: 5,
       name: "E Register",
       value: 0,
       isEdit: false,
-      key: 0,
     },
     {
       id: 6,
       name: "H Register",
       value: 0,
       isEdit: false,
-      key: 0,
     },
     {
       id: 7,
       name: "L Register",
       value: 0,
       isEdit: false,
-      key: 0,
     },
   ]);
+
+  const handleRegisterEditBtn = (register) => {
+    var datas = RegisterData;
+    datas.forEach((data) => {
+      if (data.id === register.id) {
+        data.isEdit = true;
+      }
+    });
+    setRegisterData([...datas]);
+  };
+  const handleSubmitCancel = (register) => {
+    var datas = RegisterData;
+    datas.forEach((data) => {
+      if (data.id === register.id) {
+        if (parseInt(data.value) > 255) {
+          setisErrorText(true);
+        } else {
+          var deci = data.value;
+          var hexString = parseInt(deci).toString(16);
+          data.value = hexString;
+          data.isEdit = false;
+        }
+      }
+    });
+    setRegisterData([...datas]);
+  };
+
+  const handleCancel = (register) => {
+    var datas = RegisterData;
+    datas.forEach((data) => {
+      if (data.id === register.id) {
+        data.isEdit = false;
+        data.value = 0;
+      }
+    });
+    setRegisterData([...datas]);
+  };
+
+  const handleReset = () => {
+    var datas = RegisterData;
+    datas.forEach((data) => {
+      data.isEdit = false;
+      data.value = 0;
+    });
+    setRegisterData([...datas]);
+  };
 
   return (
     <div className={style.addressWrapper}>
       <div className={style.addressTitle}>
         <p className={style.title}>Manipulate Individual Registers</p>
         <p className={style.subtitle}>Update values of individual registers</p>
+        <p className={style.subtitleSmaller}>
+          Add Values in Decimal (0 to 255) and Data will be converted to Hex
+        </p>
       </div>
       <div className={style.addressTableWrapper}>
         <Table centered hoverable responsive striped>
@@ -78,50 +120,70 @@ const ManipulateRegister = () => {
             </tr>
           </thead>
           <tbody>
-            {RegisterData.map((register) => {
-              if (register.isEdit && register.key === 1) {
-                return (
-                  <div key={register.id}>
-                    <TextInput type="number" placeholder="Enter Value" />
-                    <Button small waves style={{ margin: "20px" }}>
+            {RegisterData.map((register) => (
+              <tr>
+                {register.isEdit ? (
+                  <div>
+                    <TextInput
+                      type="number"
+                      error={isErrorText ? "only 0 to 255" : null}
+                      max="255"
+                      placeholder="Enter Value"
+                      onChange={(event) => {
+                        const data = RegisterData;
+                        data.forEach((data) => {
+                          if (data.id === register.id) {
+                            data.value = event.target.value;
+                          }
+                        });
+                        setRegisterData([...data]);
+                      }}
+                    />
+                    <Button
+                      type="submit"
+                      validate
+                      small
+                      waves
+                      style={{ margin: "20px" }}
+                      onClick={() => handleSubmitCancel(register)}
+                    >
                       Submit
                     </Button>
-                    <Button small waves className="red lighten-2">
+                    <Button
+                      small
+                      waves
+                      className="red lighten-2"
+                      onClick={() => handleCancel(register)}
+                    >
                       Cancel
                     </Button>
                   </div>
-                );
-              } else {
-                return (
-                  <tr key={register.id}>
+                ) : (
+                  <React.Fragment>
                     <td>{register.name}</td>
-                    <td>{register.value}</td>
+                    <td style={{ textTransform: "uppercase" }}>
+                      {register.value}
+                    </td>
                     <td>
                       <Button
                         floating
                         icon={<Icon>edit</Icon>}
-                        onClick={() => {
-                          setRegisterData((RegisterData) => {
-                            RegisterData.forEach((data) => {
-                              if (data.id === register.id) {
-                                data.isEdit = true;
-                                data.key = 1;
-                              }
-                            });
-                            console.log(RegisterData);
-                            return RegisterData;
-                          });
-                        }}
+                        onClick={() => handleRegisterEditBtn(register)}
                       />
                     </td>
-                  </tr>
-                );
-              }
-            })}
+                  </React.Fragment>
+                )}
+              </tr>
+            ))}
           </tbody>
         </Table>
       </div>
-      <Button node="button" className="red lighten-1" waves="light">
+      <Button
+        node="button"
+        className="red lighten-1"
+        waves="light"
+        onClick={handleReset}
+      >
         Reset
       </Button>
     </div>
