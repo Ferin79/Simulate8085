edcd9d7c213d53481684d7f88a649623dc77816b
Ferin <ferin@localhost.localdomain>
2020-05-15 07:57:59 +0530

    Updates

---
+++ precache-manifest.8f2b64d5a089701b5aa52a4f7a8aae35.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ddb7edbe516d1b34924ae827b93a19",
+    "url": "/Emulate8085/index.html"
+  },
+  {
+    "revision": "72fc72e6c55c4f95f412",
+    "url": "/Emulate8085/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "93719848fd1df0566fb9",
+    "url": "/Emulate8085/static/css/main.41d1c898.chunk.css"
+  },
+  {
+    "revision": "72fc72e6c55c4f95f412",
+    "url": "/Emulate8085/static/js/2.04610aed.chunk.js"
+  },
+  {
+    "revision": "d0b30a1909a753c393e134caf98fa9bb",
+    "url": "/Emulate8085/static/js/2.04610aed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93719848fd1df0566fb9",
+    "url": "/Emulate8085/static/js/main.2e1226a3.chunk.js"
+  },
+  {
+    "revision": "b8c2877c78114b250e24",
+    "url": "/Emulate8085/static/js/runtime-main.d42828e4.js"
+  },
+  {
+    "revision": "819d8faf1cc0ec69570bf3621bc8ab2a",
+    "url": "/Emulate8085/static/media/cpu.819d8faf.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Stimulate8085/precache-manifest.d1f0ffb91593ee46f64be34cf247095e.js"
+  "/Emulate8085/precache-manifest.8f2b64d5a089701b5aa52a4f7a8aae35.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Stimulate8085/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Emulate8085/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
